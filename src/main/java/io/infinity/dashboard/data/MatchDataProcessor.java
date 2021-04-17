01990d8a1349a9edb3a2b3f7f134e67359889bdc
Ashish Deshpande <ashishdeshpande123@gmail.com>
2021-04-17 14:58:53 +0530

    Rebased .gitignore

--- src/main/java/io/infinity/dashboard/data/MatchDataProcessor.java
+++ src/main/java/io/infinity/dashboard/data/MatchDataProcessor.java
@@ -8,6 +8,6 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
     
     @Override
     public Match process(final MatchInput matchInput) throws Exception {
-        
+        return null;
     }
 }

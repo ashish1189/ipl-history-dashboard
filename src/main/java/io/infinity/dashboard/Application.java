17b6b20ef7921d5069a65a01256ceecfc97ad4d8
Ashish Deshpande <ashishdeshpande123@gmail.com>
2021-04-18 23:34:57 +0530

    Completed spring batch job to load csv in database

--- src/main/java/io/infinity/dashboard/Application.java
+++ src/main/java/io/infinity/dashboard/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IplHistoryDashboardApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IplHistoryDashboardApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/io/infinity/dashboard/model/Match.java
+++ src/main/java/io/infinity/dashboard/model/Match.java
@@ -5,15 +5,21 @@ import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @Builder
+@Entity
 public class Match {
+
+    @Id
     private long id;
     private String city;
     private LocalDate date;
     private String playerOfMatch;
     private String venue;
-    private String neutral_venue;
+    private String neutralVenue;
     private String team1;
     private String team2;
     private String tossWinner;

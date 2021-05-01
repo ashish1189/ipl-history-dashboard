47efc9b8f76e942d9cad468e0f04f9564e65dd1f
Ashish Deshpande <ashishdeshpande123@gmail.com>
2021-05-01 16:28:48 +0530

    Functional react component with team mapping on UI

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,15 +1,29 @@
-import { React } from 'react';
-import { MatchDetailCard } from '../components/MatchDetailCard';
-import { MatchSmallCard } from '../components/MatchSmallCard';
+import { React, useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
+import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
+  const [team, setTeam] = useState({ matches: [] });
+
+  useEffect(() => {
+    const getMatches = async () => {
+      const response = await fetch(
+        "http://localhost:9091/team/Kings%20XI%20Punjab"
+      );
+
+      const data = await response.json();
+      setTeam(data);
+    };
+    getMatches();
+  }, []);
+
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>
-      <MatchDetailCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
-      <MatchSmallCard />
+      <h1>{team.teamName}</h1>
+      <MatchDetailCard match={team.matches[0]} />
+      {team.matches.slice(1).map((match) => (
+        <MatchSmallCard match={match} />
+      ))}
     </div>
   );
-}
+};
--- src/main/java/io/infinity/dashboard/controller/TeamController.java
+++ src/main/java/io/infinity/dashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import io.infinity.dashboard.model.Team;
 import io.infinity.dashboard.repository.MatchRepository;
 import io.infinity.dashboard.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
